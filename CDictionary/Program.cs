70b0adf02675983c66f0b800bcd9591c0f8ef9b2
Gustavo Boaz <gustavo.boaz@hotmail.com>
2022-07-22 10:12:26 -0300

    @GustavoBoaz Continuação aula Collections

--- CDictionary/Program.cs
+++ CDictionary/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using static CQueue.Program;
 
 namespace CDictionary
 {
@@ -6,7 +8,25 @@ namespace CDictionary
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Dicionario!");
+
+            Dictionary<string, Pessoa> dic = new Dictionary<string, Pessoa>();
+
+            dic.Add("Tuti", new Pessoa("Maju", "São Paulo", 35));
+            dic.Add("Gpam", new Pessoa("Jean", "Macedonia", 20));
+            dic.Add("Cupim", new Pessoa("Felipe", "Fortaleza", 25));
+
+            foreach(KeyValuePair<string, Pessoa> kvp in dic)
+            {
+                Console.WriteLine($"Chave = {kvp.Key} | Valor = {kvp.Value}");
+            }
+
+            Console.WriteLine($"{dic["Cupim"]}");
+
+            dic["Cupim"] = new Pessoa("Felipe Alterado","Fortaleza",0);
+
+            dic.Remove("Gpam");
+
         }
     }
 }
--- CStack/Program.cs
+++ CStack/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using static CQueue.Program;
 
 namespace CStack
 {
@@ -6,7 +8,28 @@ namespace CStack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Pilha!");
+
+            Stack<Pessoa> stack = new Stack<Pessoa>();
+
+            stack.Push(new Pessoa("Fabio", "Bruxelas", 36));
+            stack.Push(new Pessoa("Amanda", "Rio", 49));
+            stack.Push(new Pessoa("Thaina", "Barcelona", 12));
+            stack.Push(new Pessoa("Gabriel", "Tokio", 30));
+
+            Console.WriteLine($"Quantidade de pessoasl: {stack.Count}");
+
+            Console.WriteLine($"Primeira pessoa do topo: {stack.Peek()}");
+
+            stack.Pop();
+
+            Console.WriteLine($"Primeira pessoa do topo: {stack.Peek()}");
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine($"{item.ToString()}");
+
+            }
         }
     }
 }
